c2989303697b41cd3021772f5b9e8429a4cd0720
kumarnani25 <kumar.nani25@gmail.com>
2019-08-19 21:12:10 +0530

    Naveen

---
+++ samplePractiseproject/src/test/java/samplePractiseproject/openbrowser.java
@@ -0,0 +1,20 @@
+package samplePractiseproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class openbrowser 
+
+{
+
+	@Test
+	public void test()
+	{
+	System.setProperty("webdriver.chrome.driver", "D://chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.com");
+	
+	}
+	
+}
---
+++ samplePractiseproject/src/test/java/samplePractiseproject/xmltest.java
@@ -0,0 +1,44 @@
+package samplePractiseproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+
+public class xmltest 
+
+{
+
+	@Test
+	public  void Test23() 
+	{
+		XmlSuite s=new XmlSuite();
+		s.setName("Suite");
+		
+		XmlTest ts=new XmlTest(s);
+		ts.setName("Test");
+		
+		
+		
+		List<XmlClass> ls=new ArrayList<XmlClass>();
+		//XmlClass c=new XmlClass(samplePractiseproject.openbrowser);
+		//ls.add(c);
+		
+		ts.setXmlClasses(ls);
+		
+		TestNG tg=new TestNG();
+		List<XmlSuite> lst=new ArrayList<XmlSuite>();
+		lst.add(s);
+		tg.setXmlSuites(lst);
+		tg.run();
+		
+		
+				
+	}
+	
+	
+}
