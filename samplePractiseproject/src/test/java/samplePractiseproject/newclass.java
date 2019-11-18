a17fb0b386ee88e4ae9ab42d0d3588217ef066a9
kumarnani25 <kumar.nani25@gmail.com>
2019-11-18 22:06:19 +0530

    naveen

---
+++ samplePractiseproject/src/test/java/samplePractiseproject/newclass.java
@@ -0,0 +1,13 @@
+package samplePractiseproject;
+
+import org.testng.annotations.Test;
+
+public class newclass {
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Name of the test");
+	}
+
+}
